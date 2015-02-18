deeae8fb799ec532a37010a15d7a3304991f3ab7
Aaron DeVera <adevera@fordham.edu>
2015-02-17 20:41:23 -0500

    combo update

--- algorithm/combo.py
+++ algorithm/combo.py
@@ -1,3 +1,5 @@
 a = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-for i in a:
-       for j in a:
+
+for i in range(len(a)):
+       for j in range(len(a)):
+       		print a[i] + a[j]
