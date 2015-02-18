67a45e10d51d8405ce3a1a7b6f1dd51a51ac5fe2
Aaron DeVera <adevera@fordham.edu>
2015-02-18 11:22:48 -0500

    spaced

--- algorithm/combo.py
+++ algorithm/combo.py
@@ -1,5 +1,6 @@
 a = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-
+b = [26]
 for i in range(len(a)):
        for j in range(len(a)):
-       		print a[i] + a[j]
+       		b[i] a[i] + a[j]
+print b
