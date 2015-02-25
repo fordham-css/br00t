74074cd5b20af51f1ca4499a8090e6a6db94fed3
Aaron DeVera <adevera@fordham.edu>
2015-02-24 20:26:13 -0500

    cleaning godless

--- algorithm/goodlist.py
+++ algorithm/goodlist.py
@@ -9,13 +9,9 @@ def main():
         LIST = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
         result = []
         number = 8     #input("What is the maximum passwords length? ")
-        if type(number) != int:
-                print "Please enter a number."
-                main()
-        else:
-                for zz in range(number+1):
-                        for zzz in itertools.product(list1,repeat=zz):
-                                print(''.join(zzz))
+        for zz in range(number+1):
+                for zzz in itertools.product(list1,repeat=zz):
+                        print(''.join(zzz))
 
 
 
