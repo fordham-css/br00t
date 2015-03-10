1e0d3dbcd8cd77257d54fafc8910a7c253567839
Aaron DeVera <adevera@fordham.edu>
2015-03-10 00:56:11 -0400

    celery working

--- server/app.py
+++ server/app.py
@@ -1,12 +1,22 @@
 import os
 from flask import Flask, render_template, flash
-
-from flask import session, url_for, request, redirect, flash
+from flask import session, url_for, request, redirect
+from celery import Celery
 
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 app.secret_key = "dev key"
+
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379'
+
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
+@celery.task()
+def payload():
+    return """<script>console.log('hello world');</script>"""
 
 
 # Controllers
@@ -15,8 +25,10 @@ def page_not_found(error):
     return "Page not found", 404
 
 @app.route("/")
-def hello():
-    return render_template("index.html")
+def index():
+	result = payload.delay()
+	result.wait()
+	return render_template("index.html")
 
 
 
